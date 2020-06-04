416207e22945f0b9dd09ef4558e903f88ade75dc
Arune Martinkeviciute <arunemart@gmail.com>
2020-06-04 17:39:32 +0200

    added pet store

--- bin/seeds.js
+++ bin/seeds.js
@@ -2,66 +2,68 @@ let ShopModel = require('../models/Shop.model');
 const mongoose = require('mongoose');
 require('../configs/db.config');
 
-let pets = [{
-  icon: '',
-  name: 'Otter',
-  itemType: 'pet',
-  author: ''
-},
-{
-  icon: '',
-  name: 'Hippo',
-  itemType: 'pet',
-  author: ''
-},
-{
-  icon: '',
-  name: 'Cat',
-  itemType: 'pet',
-  author: ''
-},
-{
-  icon: '',
-  name: 'Spider',
-  itemType: 'pet',
-  author: ''
-},
-{
-  icon: '',
-  name: 'Kiwi Bird',
-  itemType: 'pet',
-  author: ''
-},
-{
-  icon: '',
-  name: 'Frog',
-  itemType: 'pet',
-  author: ''
-},
-{
-  icon: '',
-  name: 'Dragon',
-  itemType: 'pet',
-  author: ''
-},
-{
-  icon: '',
-  name: 'Crow',
-  itemType: 'pet',
-  author: ''
-},
-{
-  icon: '',
-  name: 'Fish',
-  itemType: 'pet',
-  author: ''
-},
-{
-  icon: '',
-  name: 'Dog',
-  itemType: 'pet',
-  author: ''
-}];
+let pets = [
+  {
+    icon: '<i class="fas fa-cat"></i>',
+    name: 'Cat',
+    itemType: 'pet',
+    author: ''
+  },
+  {
+    icon: '<i class="fas fa-otter"></i>',
+    name: 'Otter',
+    itemType: 'pet',
+    author: ''
+  },
+  {
+    icon: '<i class="fas fa-hippo"></i>',
+    name: 'Hippo',
+    itemType: 'pet',
+    author: ''
+  },
+  {
+    icon: '<i class="fas fa-spider"></i>',
+    name: 'Spider',
+    itemType: 'pet',
+    author: ''
+  },
+  {
+    icon: '<i class="fas fa-kiwi-bird"></i>',
+    name: 'Kiwi Bird',
+    itemType: 'pet',
+    author: ''
+  },
+  {
+    icon: '<i class="fas fa-frog"></i>',
+    name: 'Frog',
+    itemType: 'pet',
+    author: ''
+  },
+  {
+    icon: '<i class="fas fa-dragon"></i>',
+    name: 'Dragon',
+    itemType: 'pet',
+    author: ''
+  },
+  {
+    icon: '<i class="fas fa-crow"></i>',
+    name: 'Crow',
+    itemType: 'pet',
+    author: ''
+  },
+  {
+    icon: '<i class="fas fa-fish"></i>',
+    name: 'Fish',
+    itemType: 'pet',
+    author: ''
+  },
+  {
+    icon: '<i class="fas fa-paw"></i>',
+    name: 'Dog',
+    itemType: 'pet',
+    author: ''
+  }
+];
 
 let dataBase = ShopModel.create(pets)
 .then((response) => {
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const ShopModel = require('../models/Shop.model');
 
 /* GET home page */
 router.get('/', (req, res) => {
@@ -9,13 +10,34 @@ router.get('/profile/books', (req, res) => {
   res.render('shop/books.hbs');
 });
 router.get('/profile/pets', (req, res) => {
-  res.render('shop/pets.hbs');
+  ShopModel.find()
+  .then((pets) => {
+    res.render('shop/pets.hbs', {pets});
+  })
+  .catch(() => {
+    console.log('something went wrong');
+  });
 });
 router.get('/profile/potions', (req, res) => {
   res.render('shop/potions.hbs');
 });
 router.get('/profile/wands', (req, res) => {
   res.render('shop/wands.hbs');
+});
+
+router.post('/profile/pets/:id/delete', (req, res, next) => {
+  // Iteration #5: Delete the drone
+  ShopModel.findByIdAndDelete(req.params.id)
+  .then((response) => {
+      res.redirect('/profile/pets');
+  })
+  .catch(() => {
+      res.send('something went wrong');
+  });
+});
+
+router.get('/profile/create', (req, res) => {
+  res.render('shop/create.hbs');
 });
 
 module.exports = router;
