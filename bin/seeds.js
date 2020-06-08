aa3f74aaa15b835d696a856a42cfa8f186eb5830
James Dawson <63356869+polymurph13@users.noreply.github.com>
2020-06-08 16:53:55 +0100

    added icons to content

--- bin/seeds.js
+++ bin/seeds.js
@@ -64,138 +64,150 @@ let pets = [{
 }];
 
 let books = [{
-  icon: ' ',
+  icon: 'fas fa-book',
   name: 'A History of Magic, by Adalbert Waffling',
   itemType: 'book',
   description: 'Where it all began'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-book',
   name: 'A Beginner\'s Guide to Transfiguration, by Emeric Switch',
   itemType: 'book',
   description: 'Everything you need to know about Transfiguration'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-book-dead',
   name: 'One Thousand Magical Herbs and Fungi, by Phyllida Spore',
   itemType: 'book',
   description: 'A guide to the magical Flora surrounding us'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-book-dead',
   name: 'Magical Drafts and Potions, by Arsenius Jigger',
   itemType: 'book',
   description: 'Your guide to potions'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-book-dead',
   name: 'Fantastic Beasts and Where to Find Them, by Newt Scamander',
   itemType: 'book',
   description: 'Where to find great beasts'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-book-dead',
   name: 'The Dark Forces: A Guide to Self-Protection, by Quentin Trimble',
   itemType: 'book',
   description: 'How to protect yourself from The Dark Forces'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-book',
   name: 'The Standard Book of Spells (Grade 1), by Miranda Goshawk',
   itemType: 'book',
   description: 'Everything you need to know about spells'
 }];
 
 let potions = [{
-  icon: ' ',
+  icon: 'fas fa-flask',
   name: 'Draught of Living Death',
   itemType: 'potion',
   description: 'When a person drinks the Draught of Living Death, they go into a deep sleep so strong that they appear to be dead'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-glass-whiskey',
   name: 'Pepperup Potion',
   itemType: 'potion',
   description: 'A Pepperup Potion is designed to improve health, relieve coughs and colds'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-flask',
   name: 'Draught of Peace',
   itemType: 'potion',
   description: 'The Draught of Peace provides personal comfort, calms anxiety and soothes agitation'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-flask',
   name: 'Confusing Concoction',
   itemType: 'potion',
   description: 'A Confusing Concoction causes the drinker to become confused, distracted[3] and sick.'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-glass-martini',
   name: 'Amortentia',
   itemType: 'potion',
   description: 'Amortentia is a love potion that does not create actual love, but gives the drinker a powerful obsession and infatuation with the giver of the potion'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-flask',
   name: 'Skele-Gro',
   itemType: 'potion',
   description: 'Skele-Gro is a medicinal potion that can regrow missing or removed bones, though it tastes terrible and the process is very slow and extremely painful'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-glass-whiskey',
   name: 'Sleekeazy\'s Hair Potion',
   itemType: 'potion',
   description: 'Sleekeazy\'s Hair Potion is a beauty treatment used by witches to smooth and straighten hair'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-glass-whiskey',
   name: 'Veritaserum',
   itemType: 'potion',
   description: 'Three drops of this potion are all that is needed to force anyone to respond to any question with the truth'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-flask',
   name: 'Polyjuice Potion',
   itemType: 'potion',
   description: 'The Polyjuice Potion allows the drinker to assume the appearance of someone else for an hour or more depending on the quantity'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-flask',
   name: 'Felix Felicis',
   itemType: 'potion',
   description: 'Felix Felicis, more commonly known as Liquid Luck, grants whoever drinks it unusually good luck'
 }];
 
 let cloaks = [{
-  icon: ' ',
+  icon: 'fas fa-tshirt',
   name: 'Winter Cloak',
   itemType: 'cloak',
   description: 'Required to wear in cold weather'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-tshirt',
   name: 'Cloack of invisibility',
   itemType: 'cloak',
   description: 'This cloak renders the wearer truly invisible, as opposed to other cloaks, whose enchantment tends to wear off after years of frequent use.'
 },
 {
-  icon: ' ',
-  name: 'Shield Cloacks',
+  icon: 'fas fa-tshirt',
+  name: 'Shield Cloaks',
   itemType: 'cloak',
   description: 'These cloaks are enchanted with a Shield Charm and, when worn, shield the wearer from many light to moderate curses, jinxes, and other unfriendly spells. '
 },
 {
-  icon: ' ',
+  icon: 'fas fa-tshirt',
   name: 'Mantle',
   itemType: 'cloak',
   description: 'A type of loose garment; a long, loose cape-like cloak usually worn over indoor clothing to serve the same purpose as an overcoat'
 },
 {
-  icon: ' ',
+  icon: 'fas fa-tshirt',
   name: 'Fur Cloak',
   itemType: 'cloak',
   description: 'Long outer garment worn over the shoulders covering the back, with a fur lining.'
+},
+{
+  icon: 'fas fa-mitten',
+  name: 'Dragon Hide Gloves',
+  itemType: 'cloak',
+  description: 'I\'m sure you need it more than the dragon did'
+},
+{
+  icon: 'fas fa-hat-wizard',
+  name: 'Classic Pointed Hat',
+  itemType: 'cloak',
+  description: 'Now you can look fashionable whilst cackling'
 }];
 
 
