cf5a4336d5d9d6db75a65eba5cbbbd41345b910c
James Dawson <63356869+polymurph13@users.noreply.github.com>
2020-06-10 10:43:59 +0100

    minor changes to hp font

--- public/javascripts/layoutScript.js
+++ public/javascripts/layoutScript.js
@@ -6,6 +6,7 @@ function adjustTopMargin() {
   let titlePanel = document.getElementsByClassName('title-panel')[0];
   let mainPanel = document.getElementsByClassName('main-panel')[0];
   mainPanel.style.marginTop = getComputedStyle(titlePanel).height;
+  console.log(getComputedStyle(titlePanel).height.substring(-2));
 }
 
 document.querySelector('body').setAttribute('onresize', 'adjustTopMargin()');
